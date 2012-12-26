e458a2e6bce41f7f6fb261e77cb8b1d0c28a477d
Necrodoom <doomed.war@gmail.com>
2012-12-26 19:44:34 +0200

    Update src/com/amoebaman/blocklobber/BlockLobber.java

--- src/com/amoebaman/blocklobber/BlockLobber.java
+++ src/com/amoebaman/blocklobber/BlockLobber.java
@@ -178,7 +178,6 @@ public class BlockLobber extends JavaPlugin{
 		    presets.put(player.getName(), values);
 		    player.sendMessage(ChatColor.YELLOW + "Block lobbing preset " + ChatColor.GREEN + args[0].toLowerCase() + ChatColor.YELLOW + " updated!");
 		    return true;
-			}
 		}
 		
 		return true;
