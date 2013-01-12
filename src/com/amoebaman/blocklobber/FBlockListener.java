8ce4b6d2242ee11faa83d517c51854ddfe7d4520
Necrodoom <doomed.war@gmail.com>
2013-01-12 10:50:30 +0200

    Update src/com/amoebaman/blocklobber/FBlockListener.java

--- src/com/amoebaman/blocklobber/FBlockListener.java
+++ src/com/amoebaman/blocklobber/FBlockListener.java
@@ -1,7 +1,22 @@
 package com.amoebaman.blocklobber;
 
 import java.util.*;
+import java.util.logging.Logger;
 import org.bukkit.command.*;
 import org.bukkit.event.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
+
+public class FBlockListener extends JavaPlugin {
+ 
+    @Override
+    public void onEnable() 
+
+        getServer().getPluginManager().registerEvents(new Listener() {
+ 
+            @EventHandler
+            public fallenBlock(EntityChangeBlockEvent event) {
+                // insert debug test here
+                getLogger().info("Who calls entity a freaking 'what'" + what);
+            }
+        }, this);
