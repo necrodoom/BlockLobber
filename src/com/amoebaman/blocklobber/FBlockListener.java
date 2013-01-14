66e6f940caf22c651f04ced8f06bd15ba8528b7c
Necrodoom <doomed.war@gmail.com>
2013-01-14 16:42:18 +0200

    Update src/com/amoebaman/blocklobber/FBlockListener.java

--- src/com/amoebaman/blocklobber/FBlockListener.java
+++ src/com/amoebaman/blocklobber/FBlockListener.java
@@ -15,7 +15,7 @@ import org.bukkit.event.entity.EntityChangeBlockEvent;
 
 public class FBlockListener implements Listener {
 
-    Logger log = Logger.getLogger(FBlockListener.class.getName());
+    instance.getLogger();
     private final BlockLobber instance;
     public FBlockListener (BlockLobber instance)
     {
