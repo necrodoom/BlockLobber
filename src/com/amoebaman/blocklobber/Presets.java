c68ec5815d421f743390b19d3e3ca2bde2c50a05
Necrodoom <doomed.war@gmail.com>
2013-01-18 18:07:42 +0200

    Update src/com/amoebaman/blocklobber/Presets.java

--- src/com/amoebaman/blocklobber/Presets.java
+++ src/com/amoebaman/blocklobber/Presets.java
@@ -24,24 +24,3 @@ public class Presets implements Cloneable
 		}
 	}	
 }
-
-// ---- PROJECTILE SECTION ----
-public class ProjPresets implements Cloneable
-{
-	
-	public String projtype;
-	public byte projstrength;
-	public Location projloc;
-	public Vector projdir;
-	
-	public ProjPresets(Presets other)
-	{
-		if(other != null)
-		{			
-			projtype = other.projtype;
-			projstrength = other.projstrength;
-			projloc = other.projloc;
-			projdir = other.projdir;
-		}
-	}
-}
