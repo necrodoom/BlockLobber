002c9500414650944f31ce4fa9d02f9d0097b303
Necrodoom <doomed.war@gmail.com>
2013-01-27 19:00:16 +0200

    its a LOCATION object

--- src/com/amoebaman/blocklobber/BlockLobber.java
+++ src/com/amoebaman/blocklobber/BlockLobber.java
@@ -291,7 +291,7 @@ public class BlockLobber extends JavaPlugin{
 				player.sendMessage(ChatColor.YELLOW + "Allowed projectiles:" + ChatColor.GOLD + " arrow, skull, egg, snowball, expbottle, fireball, largefireball");
 			}
 			final Vector direction = values.projdir.multiply(speed);
-			projectile = (Projectile)values.projloc.getWorld().spawn(values.projloc.clone().add(direction.normalize()), type);
+			projectile = (Projectile)values.projloc.getWorld().spawn(values.projloc.add(direction.normalize()), type);
 			projectile.setShooter(player);
 			projectile.setVelocity(direction);
 		}
